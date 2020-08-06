4fe20ae32f71358f6d40bc2c3ffa76fd89b33743
Jeison Lopez <yeissonllespinosa@hotmail.com>
2020-08-06 16:55:48 -0500

    parcial

---
+++ js/Operaciones.js
@@ -0,0 +1,112 @@
+document.getElementById("uno").addEventListener("click",uno);
+document.getElementById("dos").addEventListener("click",dos);
+document.getElementById("tres").addEventListener("click",tres);
+document.getElementById("cuatro").addEventListener("click",cuatro);
+document.getElementById("cinco").addEventListener("click",cinco);
+document.getElementById("seis").addEventListener("click",seis);
+document.getElementById("siete").addEventListener("click",siete);
+document.getElementById("ocho").addEventListener("click",ocho);
+document.getElementById("nueve").addEventListener("click",nueve);
+document.getElementById("cero").addEventListener("click",cero);
+document.getElementById("ce").addEventListener("click",ce);
+document.getElementById("igual").addEventListener("click",igual);
+document.getElementById("suma").addEventListener("click",suma);
+document.getElementById("resta").addEventListener("click",resta);
+document.getElementById("multiplicacion").addEventListener("click",multiplicacion);
+document.getElementById("division").addEventListener("click",division);
+
+function uno(){
+let actual=document.getElementById('Operacion').innerHTML;
+let sumado = document.getElementById('uno').innerHTML;
+document.getElementById('Operacion').innerHTML=actual+sumado;
+}
+
+function dos(){
+    let actual=document.getElementById('Operacion').innerHTML;
+    let sumado = document.getElementById('dos').innerHTML;
+    document.getElementById('Operacion').innerHTML=actual+sumado;
+    }
+
+function tres(){
+let actual=document.getElementById('Operacion').innerHTML;
+let sumado = document.getElementById('tres').innerHTML;
+document.getElementById('Operacion').innerHTML=actual+sumado;
+}
+
+
+function cuatro(){
+    let actual=document.getElementById('Operacion').innerHTML;
+    let sumado = document.getElementById('cuatro').innerHTML;
+    document.getElementById('Operacion').innerHTML=actual+sumado;
+    }
+
+    
+function cinco(){
+    let actual=document.getElementById('Operacion').innerHTML;
+    let sumado = document.getElementById('cinco').innerHTML;
+    document.getElementById('Operacion').innerHTML=actual+sumado;
+    }
+
+    
+function seis(){
+    let actual=document.getElementById('Operacion').innerHTML;
+    let sumado = document.getElementById('seis').innerHTML;
+    document.getElementById('Operacion').innerHTML=actual+sumado;
+    }
+
+    
+function siete(){
+    let actual=document.getElementById('Operacion').innerHTML;
+    let sumado = document.getElementById('siete').innerHTML;
+    document.getElementById('Operacion').innerHTML=actual+sumado;
+    }
+
+    
+function ocho(){
+    let actual=document.getElementById('Operacion').innerHTML;
+    let sumado = document.getElementById('ocho').innerHTML;
+    document.getElementById('Operacion').innerHTML=actual+sumado;
+    }
+
+    
+function nueve(){
+    let actual=document.getElementById('Operacion').innerHTML;
+    let sumado = document.getElementById('nueve').innerHTML;
+    document.getElementById('Operacion').innerHTML=actual+sumado;
+    }
+
+    
+function cero(){
+    let actual=document.getElementById('Operacion').innerHTML;
+    let sumado = document.getElementById('cero').innerHTML;
+    document.getElementById('Operacion').innerHTML=actual+sumado;
+    }
+
+    
+function suma(){
+    let actual=document.getElementById('Operacion').innerHTML;
+    let sumado = document.getElementById('suma').innerHTML;
+    document.getElementById('Operacion').innerHTML=actual+sumado;
+    }
+
+
+function resta(){
+    let actual=document.getElementById('Operacion').innerHTML;
+    let sumado = document.getElementById('resta').innerHTML;
+    document.getElementById('Operacion').innerHTML=actual+sumado;
+    }
+
+
+    
+function division(){
+    let actual=document.getElementById('Operacion').innerHTML;
+    let sumado = document.getElementById('division').innerHTML;
+    document.getElementById('Operacion').innerHTML=actual+sumado;
+    }
+
+     
+function multiplicacion(){
+let actual=document.getElementById('Operacion').innerHTML;
+let sumado = document.getElementById('multiplicacion').innerHTML;
+document.getElementById('Operacion').innerHTML=actual+sumado;
+}   
